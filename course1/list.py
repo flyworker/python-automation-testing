228950d05bd0fc2b3cc4d4d026d28b4d345f624b
ccao <ccao@ecvictor.com>
2017-09-10 09:36:03 -0400

    updatec course 1 slide with operators

---
+++ course1/list.py
@@ -0,0 +1,11 @@
+mylist = []
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+mylist.append("5")
+print(mylist[0]) # prints 1
+
+
+# prints out 1,2,3
+for x in mylist:
+    print(x)
--- course1/strings.py
+++ course1/strings.py
@@ -1,4 +1,13 @@
-s = 'hi'
-print(s[1])  ## i
-print(len(s))  ## 2
-print(s + ' there')  ## hi there
+one = 1
+two = 2
+three = one + two
+print(three)
+
+hello = "hello"
+world = "world"
+helloworld = hello + " " + world
+print(helloworld)
+
+
+a, b = 3, 4
+print(a,b)
