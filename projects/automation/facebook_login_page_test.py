53a026591aad7b660f80d0cbd9c72217266f845b
Charles Cao <caotaotao@gmail.com>
2018-02-07 20:58:01 -0500

    update facebook login test

--- projects/automation/facebook_login_page_test.py
+++ projects/automation/facebook_login_page_test.py
@@ -1,25 +1,40 @@
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-user = "ecv"
-pwd = "12345"
+WAITING_TIME = 5
 
-options = webdriver.ChromeOptions()
-options.add_argument("--start-maximized")
 
-driver = webdriver.Chrome('./chromedriver',chrome_options=options)
+def login_fail_test(driver, user, pwd):
+    assert "Facebook" in driver.title
 
-driver.get("http://www.facebook.com")
+    driver.implicitly_wait(WAITING_TIME)
+    elem = driver.find_element_by_id("email")
+    elem.send_keys(user)
+    elem = driver.find_element_by_id("pass")
 
-assert "Facebook" in driver.title
-time.sleep(3)
-elem = driver.find_element_by_id("email")
-elem.send_keys(user)
-elem = driver.find_element_by_id("pass")
-elem.send_keys(pwd)
-time.sleep(5)
-elem.send_keys(Keys.RETURN)
-time.sleep(5)
-driver.close()
+    elem.send_keys(pwd)
+
+    driver.implicitly_wait(WAITING_TIME)
+
+    elem.send_keys(Keys.RETURN)
+
+    driver.implicitly_wait(WAITING_TIME)
+    # assert "Log into Facebook" in driver.title
+    assert "Log into Facebook" in driver.find_element_by_xpath('//*[@id="header_block"]/span').text
+
+
+if __name__ == "__main__":
+    user = "ecv"
+    pwd = "12345"
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("--start-maximized")
+
+    driver = webdriver.Chrome('./chromedriver', chrome_options=options)
+
+    driver.get("http://www.facebook.com")
+
+    login_fail_test(driver, user, pwd)
+
+    driver.close()
