32524220ac5a076529f97f4339217f33a51aa019
Charles Cao <caotaotao@gmail.com>
2018-07-28 18:01:35 -0400

    Add morgage calculator.

--- course4/api_test.py
+++ course4/api_test.py
@@ -1,6 +1,28 @@
 import requests
+import unittest
 
-url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
-reponse = requests.get(url)
-print(reponse.json())
-print(reponse.json()['bpi']['USD']['rate'])
+
+
+class TestStringMethods(unittest.TestCase):
+    url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
+    reponse = requests.get(url)
+
+    
+    def test_btc_price(self):
+        btc_json = self.reponse.json()
+        btc_price = btc_json['bpi']['USD']['rate']
+        self.assertIsInstance(btc_price,str)
+        btc_price_float=float(str(btc_price).replace(",", ""))
+        print(btc_price_float)
+        
+        rate_float=  btc_json['bpi']['USD']['rate_float']
+        self.assertEqual( btc_price_float,rate_float)
+
+
+    def test_btc_GPDprice(self):
+        btc_json = self.reponse.json()
+        btc_price = btc_json['bpi']['GBP']['rate']
+        self.assertIsInstance(btc_price,str)
+
+if __name__ == '__main__':
+      unittest.main()
