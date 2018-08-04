d116d9588fd660e9009ff9c70abd4d4d1dc09e9d
Charles Cao <caotaotao@gmail.com>
2018-08-04 16:28:28 -0400

    add employee db python

---
+++ course6/read_employee.py
@@ -0,0 +1,19 @@
+import pymysql
+
+
+def read_data(domain=None, user=None, password=None, db_name=None):
+    db = pymysql.connect(domain, user, password, db_name)
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
+
+    # Prepare SQL query to INSERT a record into the database.
+    sql = "SELECT * FROM ccao.person"
+    # try:
+    # Execute the SQL command
+    cursor.execute(sql)
+
+    # Fetch all the rows in a list of lists.
+    results = cursor.fetchall()
+
+if __name__ == '__main__':
+    pass
