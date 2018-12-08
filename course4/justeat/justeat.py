7507ebe6ba7af60edf3df51c0661f2b01dbb84ac
Charles Cao <ccao@nbai.io>
2018-12-08 15:05:45 -0500

    add justeat robot

--- course4/justeat/justeat.py
+++ course4/justeat/justeat.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
@@ -15,60 +14,24 @@ class AppDynamicsJob(unittest.TestCase):
         # as documented in https://docs.appdynamics.com/display/PRO44/Write+Your+First+Script
         self.driver = webdriver.Chrome("./chromedriver")
         self.driver.implicitly_wait(30)
-        self.base_url = "https://www.just-eat.ca/"
+        self.base_url = "https://www.katalon.com/"
         self.verificationErrors = []
         self.accept_next_alert = True
 
     def test_app_dynamics_job(self):
         driver = self.driver
+        driver.get("https://www.skipthedishes.com/")
 
-        driver.get(self.base_url)
-        driver.find_element_by_xpath('//*[@id="mm-modal"]/div/button').click()
+        driver.find_element_by_name("address_display").click()
+        driver.find_element_by_name("address_display").clear()
+        driver.find_element_by_name("address_display").send_keys("Rue Sherbrooke Ouest, 666")
         time.sleep(2)
-        driver.find_element_by_id("homepage-search-fullAddress").send_keys(
-            "Rue Sherbrooke Ouest, 666, H3A 1E4, Montréal")
-        time.sleep(2)
-        driver.find_element_by_xpath("//*[@id='skipToMain']/div[2]/div/div/button/span").click()
-        driver.find_element_by_xpath(
-            '//*[@id="searchResults"]//header/h3[contains(text(),"Pizza Italia")]').click()
-
-        title =driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Pizza Italia (St Laurent)'])[1]/following::h1[1]")
-
-        self.assertEqual("Pizza Italia (St Laurent)",title.text)
-
-        found_trio1=False
-        for i in range(5):
-            if found_trio1:
-                break
-            else:
-                driver.execute_script("window.scrollTo(0, 10)")
-                time.sleep(1)
-                try:
-                    # select combo
-                    driver.find_element_by_xpath(
-                        '//*[@id="container-menu--card"]/section[1]/div/div[1]/h4').click()
-                    time.sleep(1)
-                    driver.find_element_by_xpath(
-                        '//*[@id="menuContainer"]/div[2]/div[2]/div/div[3]').click()
-                    time.sleep(1)
-                    driver.find_element_by_xpath(
-                        '//*[@id="addOptionsToBasket"]/button').click()
-                    time.sleep(1)
-                    # select salade
-                    driver.find_element_by_xpath(
-                        '//*[@id="container-menu--card"]/section[3]/div/div[1]/div[2]/button/div').click()
-                    time.sleep(1)
-                    found_trio1 = True
-                except Exception:
-                    print("Did not find trio1 yet, scrolling down...")
-
-
-
-
-        self.assertEqual("$24.87",driver.find_element_by_xpath('/html/body/header/div[2]/div[2]/div/div[1]').text)
-        time.sleep(5)
-
-
+        driver.find_element_by_xpath('//*[@id="restaurant-search-form"]/div/div[1]/div/div[1]/div/div[1]/ul/li[1]/a').click()
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Reset Pin'])[1]/following::button[1]").click()
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='No restaurants found matching these filters.'])[1]/following::div[7]").click()
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Close'])[2]/following::h1[1]").click()
+        try: self.assertEqual(u"Ta Pies (cuisine néo-zélandaise et australienne) 9.9", driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Close'])[2]/following::h1[1]").text)
+        except AssertionError as e: self.verificationErrors.append(str(e))
     def is_element_present(self, how, what):
         try:
             self.driver.find_element(by=how, value=what)
---
+++ course4/justeat/justeat.robot
@@ -0,0 +1,17 @@
+*** Settings ***
+Library           OperatingSystem
+Library           justeat_lib/OrderLibrary.py
+Library           SeleniumLibrary
+Library           String
+Test Teardown  Close test browser
+*** Variables ***
+${ADDRESS}              Rue Sherbrooke Ouest, 666
+${STORE_NAME}           Ta Pies (cuisine néo-zélandaise et australienne) 9.9
+
+*** Test Cases ***
+User can find a store close to the location
+    [Tags]    UAT
+    User Enter Address in Search Box    ${ADDRESS}
+    Chose the first Store avaliable
+    Store name should be    ${STORE_NAME}
+    Close all browsers
